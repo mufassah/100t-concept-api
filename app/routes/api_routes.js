13dee2bd28ac4aae77019c3d3bd40b09a0c14960
amazonsite1 <idoesamazon@gmail.com>
2017-12-23 18:33:59 -0500

    testing

--- app/routes/api_routes.js
+++ app/routes/api_routes.js
@@ -2,20 +2,21 @@ var axios = require('axios')
 var Twitter = require('twitter')
 module.exports = function(app) {
 	app.get('/twitter', (req,res) => {
-		var client = new Twitter({
-			consumer_key: 'uyR81RFWn2zVjf1mKyg5AhQGC',
-			consumer_secret: 'BscbPybQIEhgUC9GFFWe0nHpgRxHD11TUk7qcLNDhfA4TtfWzz',
-			bearer_token: 'AAAAAAAAAAAAAAAAAAAAAJWu3gAAAAAARxdW5nqaAH7xwXI3nXjno1y0eTY%3Dnf8IqWsiegiw465OPQcwNfkQDzZUuYptLawh2jm8AHcGECzpFA'
-		})
-		client.get('statuses/user_timeline', {screen_name: '100thieves', count: 1}, function(error, tweets, response){
-			if(!error) {
-				console.log(tweets)
-				res.send(tweets)
-			}
-			else {
-				console.log(error)
-			}
+		// var client = new Twitter({
+		// 	consumer_key: 'uyR81RFWn2zVjf1mKyg5AhQGC',
+		// 	consumer_secret: 'BscbPybQIEhgUC9GFFWe0nHpgRxHD11TUk7qcLNDhfA4TtfWzz',
+		// 	bearer_token: 'AAAAAAAAAAAAAAAAAAAAAJWu3gAAAAAARxdW5nqaAH7xwXI3nXjno1y0eTY%3Dnf8IqWsiegiw465OPQcwNfkQDzZUuYptLawh2jm8AHcGECzpFA'
+		// })
+		// client.get('statuses/user_timeline', {screen_name: '100thieves', count: 1}, function(error, tweets, response){
+		// 	if(!error) {
+		// 		console.log(tweets)
+		// 		res.send(tweets)
+		// 	}
+		// 	else {
+		// 		console.log(error)
+		// 	}
 
-		})
+		// })
+		res.send('Hello')
 	})
 }
