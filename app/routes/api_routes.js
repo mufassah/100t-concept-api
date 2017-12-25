537fff43317094744a42f3ead924e2d154968e0f
amazonsite1 <idoesamazon@gmail.com>
2017-12-25 14:37:10 -0500

    7 accounts

--- app/routes/api_routes.js
+++ app/routes/api_routes.js
@@ -1,25 +1,122 @@
 var axios = require('axios')
 var Twitter = require('twitter')
 module.exports = function(app) {
-	app.get('/twitter', (req,res) => {
-		res.setHeader('Access-Control-Allow-Origin', '*');
-	    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-	    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype'); // If needed
-	    res.setHeader('Access-Control-Allow-Credentials', true);
-		var client = new Twitter({
-			consumer_key: 'uyR81RFWn2zVjf1mKyg5AhQGC',
-			consumer_secret: 'BscbPybQIEhgUC9GFFWe0nHpgRxHD11TUk7qcLNDhfA4TtfWzz',
-			bearer_token: 'AAAAAAAAAAAAAAAAAAAAAJWu3gAAAAAARxdW5nqaAH7xwXI3nXjno1y0eTY%3Dnf8IqWsiegiw465OPQcwNfkQDzZUuYptLawh2jm8AHcGECzpFA'
-		})
-		client.get('statuses/user_timeline', {screen_name: '100thieves', count: 1}, function(error, tweets, response){
-			if(!error) {
-				console.log(tweets)
-				res.send(tweets)
-			}
-			else {
-				console.log(error)
-			}
+  app.get('/twitter', async (req,res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype'); // If needed
+      res.setHeader('Access-Control-Allow-Credentials', true);
+      var client = new Twitter({
+        consumer_key: 'uyR81RFWn2zVjf1mKyg5AhQGC',
+        consumer_secret: 'BscbPybQIEhgUC9GFFWe0nHpgRxHD11TUk7qcLNDhfA4TtfWzz',
+        bearer_token: 'AAAAAAAAAAAAAAAAAAAAAJWu3gAAAAAARxdW5nqaAH7xwXI3nXjno1y0eTY%3Dnf8IqWsiegiw465OPQcwNfkQDzZUuYptLawh2jm8AHcGECzpFA'
+      })
+      let hundredTweets = []
+      await client.get('statuses/user_timeline', {screen_name: '100thieves', count: 1})
+      .then((tweets) => {
+        let twitterObj = {}
 
-		})
-	})
+        twitterObj.created = tweets[0].created_at
+        twitterObj.msg = tweets[0].text
+        twitterObj.avi = tweets[0].user.profile_image_url_https
+        twitterObj.name = tweets[0].user.name
+        hundredTweets.push(twitterObj)
+        
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+
+      await client.get('statuses/user_timeline', {screen_name: 'Nadeshot', count: 1})
+      .then((tweets) => {
+        let twitterObj = {}
+
+        twitterObj.created = tweets[0].created_at
+        twitterObj.msg = tweets[0].text
+        twitterObj.avi = tweets[0].user.profile_image_url_https
+        twitterObj.name = tweets[0].user.name
+        hundredTweets.push(twitterObj)
+        
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+
+      await client.get('statuses/user_timeline', {screen_name: 'cavsdan', count: 1})
+      .then((tweets) => {
+        let twitterObj = {}
+
+        twitterObj.created = tweets[0].created_at
+        twitterObj.msg = tweets[0].text
+        twitterObj.avi = tweets[0].user.profile_image_url_https
+        twitterObj.name = tweets[0].user.name
+        hundredTweets.push(twitterObj)
+        
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+
+      await client.get('statuses/user_timeline', {screen_name: 'pr0llyLOL', count: 1})
+      .then((tweets) => {
+        let twitterObj = {}
+
+        twitterObj.created = tweets[0].created_at
+        twitterObj.msg = tweets[0].text
+        twitterObj.avi = tweets[0].user.profile_image_url_https
+        twitterObj.name = tweets[0].user.name
+        hundredTweets.push(twitterObj)
+        
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+
+      await client.get('statuses/user_timeline', {screen_name: 'aphromoo', count: 1})
+      .then((tweets) => {
+        let twitterObj = {}
+
+        twitterObj.created = tweets[0].created_at
+        twitterObj.msg = tweets[0].text
+        twitterObj.avi = tweets[0].user.profile_image_url_https
+        twitterObj.name = tweets[0].user.name
+        hundredTweets.push(twitterObj)
+        
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+
+      await client.get('statuses/user_timeline', {screen_name: 'c9_meteos', count: 1})
+      .then((tweets) => {
+        let twitterObj = {}
+
+        twitterObj.created = tweets[0].created_at
+        twitterObj.msg = tweets[0].text
+        twitterObj.avi = tweets[0].user.profile_image_url_https
+        twitterObj.name = tweets[0].user.name
+        hundredTweets.push(twitterObj)
+        
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+
+      await client.get('statuses/user_timeline', {screen_name: 'NICKMERCS', count: 1})
+      .then((tweets) => {
+        let twitterObj = {}
+
+        twitterObj.created = tweets[0].created_at
+        twitterObj.msg = tweets[0].text
+        twitterObj.avi = tweets[0].user.profile_image_url_https
+        twitterObj.name = tweets[0].user.name
+        hundredTweets.push(twitterObj)
+        
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+
+      res.send(hundredTweets)
+    })
 }
